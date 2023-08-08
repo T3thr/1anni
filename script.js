2b12870f1d34bb40fa27e598ffdff71d2dd858bb
T3thr <141127208+T3thr@users.noreply.github.com>
2023-08-09 01:03:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,41 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const commentList = document.getElementById("comment-list");
-  const commentInput = document.getElementById("comment-input");
-  const submitButton = document.getElementById("submit-button");
-
-  // Initialize Firebase
-  firebase.initializeApp({
+firebase.initializeApp({
     apiKey: "AIzaSyDCS6hFxkfaqyTWvO7Zlo23zDbAWh8U3Oc",
     authDomain: "anni-e336f.firebaseapp.com",
     projectId: "anni-e336f",
-  });
+    storageBucket: "anni-e336f.appspot.com",
+    messagingSenderId: "773257785211",
+    appId: "1:773257785211:web:7735061a858604eb7757de",
+};
 
-  const db = firebase.firestore();
-  const auth = firebase.auth();
+firebase.initializeApp(firebaseConfig);
 
-  submitButton.addEventListener("click", function () {
-    const commentText = commentInput.value;
-    if (commentText !== "") {
-      // Add comment to Firestore
-      db.collection("comments").add({
-        text: commentText,
-        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-        userId: auth.currentUser ? auth.currentUser.uid : null,
-      });
+const db = firebase.firestore();
 
-      commentInput.value = "";
-    }
-  });
-
-  // Listen for changes in the comments collection and update the UI
-  db.collection("comments")
-    .orderBy("timestamp")
-    .onSnapshot(function (snapshot) {
-      commentList.innerHTML = "";
-      snapshot.forEach(function (doc) {
-        const commentItem = document.createElement("li");
-        commentItem.className = "comment";
-        commentItem.textContent = doc.data().text;
-
-        // Show delete button if the current user is the owner
-        if (auth.currentUser && doc.data().userId === auth.currentUser.uid) {
-          const deleteButton = document.createElement("button");
-          deleteButton.textContent = "Delete";
-          deleteButton.addEventListener("click", function () {
-            db.collection("comments").doc(doc.id).delete();
-          });
-          commentItem.appendChild(deleteButton);
-        }
-
-        commentList.appendChild(commentItem);
-      });
+// Display existing comments
+const commentsDiv = document.getElementById("comments");
+db.collection("comments")
+  .orderBy("timestamp", "desc")
+  .onSnapshot(snapshot => {
+    commentsDiv.innerHTML = "";
+    snapshot.forEach(doc => {
+      const commentData = doc.data();
+      const commentDiv = document.createElement("div");
+      commentDiv.className = "comment";
+      commentDiv.textContent = commentData.text;
+      commentsDiv.appendChild(commentDiv);
     });
-
-  // User authentication state change listener
-  auth.onAuthStateChanged(function (user) {
-    // Show/hide comment input and submit button based on authentication
-    if (user) {
-      commentInput.style.display = "block";
-      submitButton.style.display = "block";
-    } else {
-      commentInput.style.display = "none";
-      submitButton.style.display = "none";
-    }
   });
+
+// Submit a new comment
+const submitButton = document.getElementById("submitComment");
+const commentInput = document.getElementById("commentInput");
+submitButton.addEventListener("click", () => {
+  const commentText = commentInput.value.trim();
+  if (commentText !== "") {
+    db.collection("comments").add({
+      text: commentText,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    });
+    commentInput.value = "";
+  }
 });
