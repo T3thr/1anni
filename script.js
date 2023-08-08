31b28891e110b4984f7c519710cf932a426d756a
T3thr <141127208+T3thr@users.noreply.github.com>
2023-08-08 23:42:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,16 @@
-document.getElementById('wish-form').addEventListener('submit', function(event) {
-  event.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
+  const commentList = document.getElementById("comment-list");
+  const commentInput = document.getElementById("comment-input");
+  const submitButton = document.getElementById("submit-button");
 
-  const name = document.getElementById('name').value;
-  const message = document.getElementById('message').value;
-  const pictureInput = document.getElementById('picture');
-  const pictureFile = pictureInput.files[0]; // Get the first selected file (if any)
-
-  if (message.trim() !== '') {
-    const wishList = document.getElementById('wish-list');
-    const newWish = document.createElement('div');
-    newWish.classList.add('wish');
-
-    let wishContent = '';
-    if (name !== '') {
-      wishContent += `<strong>${name}:</strong> `;
+  submitButton.addEventListener("click", function () {
+    const commentText = commentInput.value;
+    if (commentText !== "") {
+      const commentItem = document.createElement("li");
+      commentItem.className = "comment";
+      commentItem.textContent = commentText;
+      commentList.appendChild(commentItem);
+      commentInput.value = "";
     }
-    wishContent += message;
-
-    if (pictureFile) {
-      const pictureReader = new FileReader();
-      pictureReader.onload = function() {
-        const pictureURL = pictureReader.result;
-        wishContent += `<br><img src="${pictureURL}" alt="Wish Picture">`;
-        newWish.innerHTML = wishContent;
-        wishList.appendChild(newWish);
-        showThankYouAlert();
-        saveWish(name, message, pictureURL);
-      };
-      pictureReader.readAsDataURL(pictureFile);
-    } else {
-      newWish.innerHTML = wishContent;
-      wishList.appendChild(newWish);
-      showThankYouAlert();
-      saveWish(name, message);
-    }
-
-    // Clear input fields after submission
-    document.getElementById('name').value = '';
-    document.getElementById('message').value = '';
-    pictureInput.value = '';
-  }
+  });
 });
-
-function showThankYouAlert() {
-  alert('Thank you for your wishes!');
-}
-
-function saveWish(name, message, pictureURL) {
-  const wish = {
-    name: name || 'Anonymous',
-    message,
-    pictureURL
-  };
-
-  let wishes = JSON.parse(localStorage.getItem('wishes'));
-  if (wishes) {
-    wishes.push(wish);
-  } else {
-    wishes = [wish];
-  }
-  localStorage.setItem('wishes', JSON.stringify(wishes));
-}
