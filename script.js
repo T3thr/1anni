cd9c0ce52582d582f68526f950525489bd823168
T3thr <141127208+T3thr@users.noreply.github.com>
2023-08-14 16:47:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-// Fetch Google Sheet data (sample data)
-const roomData = [
-    { roomNumber: "101", availability: "Available" },
-    { roomNumber: "102", availability: "Booked" },
-    // Add more room data as needed
-];
+// Fetch Google Sheet data using Google Sheets API
+function fetchSheetData() {
+    // Replace with your own Google Sheets API key and spreadsheet ID
+    const apiKey = "AIzaSyDCS6hFxkfaqyTWvO7Zlo23zDbAWh8U3Oc";
+    const spreadsheetId = "1_moyVZ-HgihXBqrvqW2zEsRtn8dEN-5CtMpBnvhEI9s/edit#gid=0";
 
-// Populate the table with Google Sheet data
-const tableBody = document.getElementById("tableBody");
-roomData.forEach(room => {
-    const row = tableBody.insertRow();
-    row.insertCell(0).textContent = room.roomNumber;
-    row.insertCell(1).textContent = room.availability;
-});
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/Sheet1?key=${apiKey}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const rows = data.values.slice(1); // Skip header row
+            const tableBody = document.getElementById("tableBody");
+
+            tableBody.innerHTML = ""; // Clear existing rows
+
+            rows.forEach(row => {
+                const roomNumber = row[0];
+                const availability = row[1];
+                const newRow = tableBody.insertRow();
+                newRow.insertCell(0).textContent = roomNumber;
+                newRow.insertCell(1).textContent = availability;
+            });
+        })
+        .catch(error => {
+            console.error("Error fetching sheet data:", error);
+        });
+}
+
+// Call fetchSheetData to load Google Sheet data when the page loads
+fetchSheetData();
