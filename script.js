cd23aed8cd7eebbee82951f1460262d231ec4e73
T3thr <141127208+T3thr@users.noreply.github.com>
2023-08-14 16:41:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,14 @@
-firebase.initializeApp({
-    apiKey: "AIzaSyDCS6hFxkfaqyTWvO7Zlo23zDbAWh8U3Oc",
-    authDomain: "anni-e336f.firebaseapp.com",
-    projectId: "anni-e336f",
-    storageBucket: "anni-e336f.appspot.com",
-    messagingSenderId: "773257785211",
-    appId: "1:773257785211:web:7735061a858604eb7757de",
-};
+// Fetch Google Sheet data (sample data)
+const roomData = [
+    { roomNumber: "101", availability: "Available" },
+    { roomNumber: "102", availability: "Booked" },
+    // Add more room data as needed
+];
 
-firebase.initializeApp(firebaseConfig);
-
-const db = firebase.firestore();
-
-// Display existing comments
-const commentsDiv = document.getElementById("comments");
-db.collection("comments")
-  .orderBy("timestamp", "desc")
-  .onSnapshot(snapshot => {
-    commentsDiv.innerHTML = "";
-    snapshot.forEach(doc => {
-      const commentData = doc.data();
-      const commentDiv = document.createElement("div");
-      commentDiv.className = "comment";
-      commentDiv.textContent = commentData.text;
-      commentsDiv.appendChild(commentDiv);
-    });
-  });
-
-// Submit a new comment
-const submitButton = document.getElementById("submitComment");
-const commentInput = document.getElementById("commentInput");
-submitButton.addEventListener("click", () => {
-  const commentText = commentInput.value.trim();
-  if (commentText !== "") {
-    db.collection("comments").add({
-      text: commentText,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp()
-    });
-    commentInput.value = "";
-  }
+// Populate the table with Google Sheet data
+const tableBody = document.getElementById("tableBody");
+roomData.forEach(room => {
+    const row = tableBody.insertRow();
+    row.insertCell(0).textContent = room.roomNumber;
+    row.insertCell(1).textContent = room.availability;
 });
